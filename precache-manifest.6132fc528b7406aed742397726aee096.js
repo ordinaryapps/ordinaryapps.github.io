19d14d7bbce51d3731b7104ba087ec5ae3d5212b
Dylan Theriot <dylantheriot@gmail.com>
2020-09-12 23:53:53 -0500

    init prankme

---
+++ precache-manifest.6132fc528b7406aed742397726aee096.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5735be00b5f6f3d2b0cf27be6a5ae840",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b424fe9669ddd6ddd003",
+    "url": "./static/css/main.98d6520d.chunk.css"
+  },
+  {
+    "revision": "1e7c96090b9b96d85433",
+    "url": "./static/js/2.db61cf6c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.db61cf6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b424fe9669ddd6ddd003",
+    "url": "./static/js/main.4e6f34f4.chunk.js"
+  },
+  {
+    "revision": "fce109da6ed7b062c220",
+    "url": "./static/js/runtime-main.fb9ccc60.js"
+  },
+  {
+    "revision": "504a6225a1a92111cf0409a7b0042bdb",
+    "url": "./static/media/laughing.504a6225.svg"
+  },
+  {
+    "revision": "97ff7f7c9c786a726ae513e6a92c1888",
+    "url": "./static/media/logo.97ff7f7c.png"
+  },
+  {
+    "revision": "df9e8baae2ee6ae1be30fbc475a756f2",
+    "url": "./static/media/logo.df9e8baa.jpg"
+  },
+  {
+    "revision": "bab61aa6e9c6150f8bfbc146381fc42a",
+    "url": "./static/media/patrick.bab61aa6.gif"
+  },
+  {
+    "revision": "dec9247ec837396cf30525cedb81182d",
+    "url": "./static/media/search.dec9247e.svg"
+  },
+  {
+    "revision": "b477f803816ae259e59cc3d65a787acd",
+    "url": "./static/media/temp_logo.b477f803.jpg"
+  },
+  {
+    "revision": "dfa8cbd43d69ed741ffbc7120585dac9",
+    "url": "./static/media/temp_logo2.dfa8cbd4.png"
+  }
+]);
